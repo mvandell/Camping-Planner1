b1093eea5f8789045592fc332b621302816eaff6
Marisa <marisavandellos@gmail.com>
2024-08-02 15:35:50 -0700

    added bullets to the list of activities

--- src/client/components/CampgroundPage/CampgroundPage.jsx
+++ src/client/components/CampgroundPage/CampgroundPage.jsx
@@ -5,17 +5,18 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Alert from "@mui/material/Alert";
 import Button from '@mui/material/Button';
+import CircleIcon from '@mui/icons-material/Circle';
 
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams,useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import { useGetSingleCampgroundQuery, usePatchCampgroundMutation, useDeleteCampgroundMutation } from "../../redux/api";
 import { usePatchCampgroundActivityAddMutation, usePatchCampgroundActivityRemoveMutation, useGetAdminQuery } from "../../redux/api";
 
 const CampgroundPage = () => {
     const token = useSelector((state) => state.auth.token)
-    const {id} = useParams();
+    const { id } = useParams();
     const navigate = useNavigate();
 
     const { data, error, isLoading } = useGetSingleCampgroundQuery(id);
@@ -31,13 +32,13 @@ const CampgroundPage = () => {
 
     return (
         <div>
-            <Typography variant="h1" sx={{color: "beige"}}>
+            <Typography variant="h1" sx={{ color: "beige" }}>
                 <a href={data.website} target="_blank" className="park">{data.park}</a>
             </Typography>
             <Grid container>
                 <Grid item xs={6}> {/* picture and info */}
                     <Card sx={{ m: 1, p: 3, backgroundColor: "linen" }}>
-                        <img src={data.picture} alt={data.park}/>
+                        <img src={data.picture} alt={data.park} />
                     </Card>
                     <Grid container>
                         <Grid item>
@@ -70,18 +71,18 @@ const CampgroundPage = () => {
                         </Grid>
                     </Grid>
                     {token && adminData.isAdmin &&
-                    <Button
-                    variant="contained"
-                    color="error"
-                    sx={{m:1, fontWeight:"bold"}}
-                    onClick={() => {
-                        if (confirm("Are you sure you want to delete this campground?") === true) {
-                            deleteCampground(data.id);
-                            navigate("/campgrounds");
-                        }
-                    }}>
-                        Delete Campground
-                    </Button>
+                        <Button
+                            variant="contained"
+                            color="error"
+                            sx={{ m: 1, fontWeight: "bold" }}
+                            onClick={() => {
+                                if (confirm("Are you sure you want to delete this campground?") === true) {
+                                    deleteCampground(data.id);
+                                    navigate("/campgrounds");
+                                }
+                            }}>
+                            Delete Campground
+                        </Button>
                     }
                 </Grid>
                 <Grid item xs={6}> {/* activities and drive info */}
@@ -91,12 +92,15 @@ const CampgroundPage = () => {
                         </Typography>
                         {data.activities && data.activities.map((activity) => (
                             <Box key={activity.id} sx={{ m: 1 }}>
-                                <Typography>{activity.name}</Typography>
+                                <Stack direction="row">
+                                    <CircleIcon sx={{fontSize: 9, p: 1}} />
+                                    <Typography>{activity.name}</Typography>
+                                </Stack>
                             </Box>
                         ))}
                     </Card>
                     <Card sx={{ m: 1, p: 1, backgroundColor: "linen" }}>
-                        <Typography variant="h5" fontWeight="bold" sx={{mb:1}}>
+                        <Typography variant="h5" fontWeight="bold" sx={{ mb: 1 }}>
                             Drive Info
                         </Typography>
                         <Typography>
