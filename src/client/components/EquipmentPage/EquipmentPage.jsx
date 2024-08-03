935f6b75e5a9498a5dbb5c745e2cb603edfeb2f4
Marisa <marisavandellos@gmail.com>
2024-08-03 16:00:25 -0700

    added new equipment form to equipment page

--- src/client/components/EquipmentPage/EquipmentPage.jsx
+++ src/client/components/EquipmentPage/EquipmentPage.jsx
@@ -3,28 +3,32 @@ import Typography from '@mui/material/Typography';
 import Stack from "@mui/material/Stack"
 import Grid from "@mui/material/Grid"
 import Alert from "@mui/material/Alert";
+import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
 
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
-import { useGetAllEquipmentQuery, useDeleteEquipmentMutation, usePostEquipmentMutation, useGetAdminQuery } from "../../redux/api";
-import { usePatchEquipmentMutation, usePatchEquipmentPackToggleMutation, usePatchEquipmentNeedToggleMutation } from "../../redux/api";
+import { useGetAllEquipmentQuery, useDeleteEquipmentMutation, useGetAdminQuery } from "../../redux/api";
+import { usePostEquipmentMutation, usePatchEquipmentPackToggleMutation, usePatchEquipmentNeedToggleMutation } from "../../redux/api";
+import NewEquipment from "./NewEquipment";
 
 const EquipmentPage = () => {
     const token = useSelector((state) => state.auth.token)
 
+    const [alert, setAlert] = useState(false);
     const [name, setName] = useState("");
 
     const { data, error, isLoading } = useGetAllEquipmentQuery();
     const { data: adminData, error: adminError, isLoading: adminIsLoading } = useGetAdminQuery();
     const [deleteEquipment] = useDeleteEquipmentMutation();
-    //const [patchEquipment] = usePatchEquipmentMutation();
+    const [postEquipment] = usePostEquipmentMutation();
     const [packToggle] = usePatchEquipmentPackToggleMutation();
     //const [needToggle] = usePatchEquipmentNeedToggleMutation();
-    const [postEquipment] = usePostEquipmentMutation();
+
 
     if (isLoading) {
         return <div> </div>;
@@ -32,7 +36,17 @@ const EquipmentPage = () => {
     if (error) {
         return <div>Error:{error.message}</div>;
     }
-    
+
+    const handleSubmit = async (event) => {
+        try {
+            event.preventDefault();
+            const response = await postEquipment({ name: name });
+            setAlert(false)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <div>
             <Typography variant="h1">
@@ -47,14 +61,14 @@ const EquipmentPage = () => {
                             <Stack direction="row">
                                 {token && adminData.isAdmin &&
                                     <IconButton
-                                    color="error"
-                                    sx={{pl: 0}}
-                                    onClick={() => {
-                                        if (confirm("Are you sure you want to delete this equipment?") === true) {
-                                            deleteEquipment(equipment.id)
-                                        }
-                                    }}>
-                                        <DeleteIcon fontSize="small"/>
+                                        color="error"
+                                        sx={{ pl: 0 }}
+                                        onClick={() => {
+                                            if (confirm("Are you sure you want to delete this equipment?") === true) {
+                                                deleteEquipment(equipment.id)
+                                            }
+                                        }}>
+                                        <DeleteIcon fontSize="small" />
                                     </IconButton>
                                 }
                                 <Typography sx={{ flexGrow: 1, py: 1 }}>
@@ -76,11 +90,36 @@ const EquipmentPage = () => {
                         </Card>
                     ))}
                 </Grid>
-                <Grid item xs={4}></Grid>
+                <Grid item xs={4}>
+                    {token && adminData.isAdmin &&
+                        <Typography textAlign="center">
+                            <Button variant="contained" onClick={() => setAlert(true)} sx={{ textTransform: "none" }}>
+                                New Equipment
+                            </Button>
+                        </Typography>
+                    }
+                    {alert && //new equipment form component
+                        <Card sx={{ p: 2, textAlign: "center", mx: 3, my: 1 }}>
+                            <form onSubmit={handleSubmit}>
+                                <TextField
+                                    label="New Equipment"
+                                    value={name}
+                                    size="small"
+                                    sx={{ m: 1 }}
+                                    onChange={(event) => setName(event.target.value)} />
+                                <Button type="submit" variant="contained" sx={{ textTransform: "none", m: 1 }}>
+                                    Submit
+                                </Button>
+                                <Button variant="contained" onClick={() => setAlert(false)} sx={{ textTransform: "none", m: 1 }}>
+                                    Cancel
+                                </Button>
+                            </form>
+                        </Card>
+                    }
+                </Grid>
             </Grid>
 
         </div>
     )
 }
-
 export default EquipmentPage
